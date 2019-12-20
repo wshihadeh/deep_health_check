2b03f8e88ccf3d15731f94ec331c56831337c0f7
Al-waleed shihadeh <wshihadeh.dev@gmail.com>
2019-12-20 09:53:38 +0100

    Init message

---
+++ lib/deep_health_check.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'deep_health_check/version'
+require 'deep_health_check/health_check'
+require 'deep_health_check/db_health_check'
+require 'deep_health_check/dependency_health_check'
+require 'deep_health_check/tcp_dependency_health_check'
+require 'deep_health_check/http_dependency_health_check'
+require 'deep_health_check/health_check_builder'
+require 'deep_health_check/middleware_health_check'
+
+module DeepHealthCheck
+end
